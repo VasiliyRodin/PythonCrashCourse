66656df64100ce18a6a6122ce0569fa726957103
Vasiliy Rodin <rodinvasiliy@gmail.com>
2025-03-14 01:06:04 -0700

    first commit

---
+++ name_cases.py
@@ -0,0 +1,23 @@
+name = "Eric"
+
+print("Hello " + name + " would you like")
+
+
+name2 = "rocky marciano"
+print ( name2.lstrip() + "\n" + name2.rstrip() + "\n" + name2.title())
+
+famous_person = "Ricky Bobby"
+message = "Save me Tom Cruise"
+
+print(famous_person + " once said, " + '"'+  message + '"') 
+
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+print (message)
+
+print (5+3)
+print (2*4)
+print (16/2)
+print (9-1)
+
+#coments
---
+++ python_work.py
@@ -0,0 +1,7 @@
+print("Hello World")
+
+message = ("Hello world again")
+print(message)
+
+print(message.upper())
+print(message.lower())
