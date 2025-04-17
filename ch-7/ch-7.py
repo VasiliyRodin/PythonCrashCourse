70e460fccd86601c87571e24062afc9193f89182
Vasiliy Rodin <rodinvasiliy@gmail.com>
2025-04-16 20:44:15 -0700

    ch-7-1

---
+++ ch-7/ch-7.py
@@ -0,0 +1,17 @@
+some_text = ''
+print("please input some text: ")
+some_text = input() 
+print("You typed: " + some_text)
+
+
+another_text = input("Plase put some text and ill copy it: ")
+print(another_text)
+
+prompt = "This program copies stuff and adds the prompt to the output"
+prompt += "This is another thign getting added to the prompt"
+name = input(prompt)
+print('\n' + name)
+
+print('\n \n\n using int() for inputing numbers only')
+
+
