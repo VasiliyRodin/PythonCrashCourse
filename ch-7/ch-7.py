dd12d9da804d2ab36271a7e96b452b0561d90bb1
Vasiliy Rodin <rodinvasiliy@gmail.com>
2025-05-05 15:07:02 -0700

    ch-7-pizza

--- ch-7/ch-7.py
+++ ch-7/ch-7.py
@@ -3,7 +3,6 @@ print("please input some text: ")
 some_text = input() 
 print("You typed: " + some_text)
 
-
 another_text = input("Plase put some text and ill copy it: ")
 print(another_text)
 
@@ -11,9 +10,6 @@ prompt = "This program copies stuff and adds the prompt to the output"
 prompt += "This is another thign getting added to the prompt"
 name = input(prompt)
 print('\n' + name)
-
-
-
 
 print('\n \n\n using int() for inputing numbers only')
 
